d04b6f6f536b28034653cf860ba868bfadf9bc3a
Chijioke Ndubisi <cjndubisi@gmail.com>
2020-05-18 15:32:55 +0100

    feat(auth): add config to parse .env files

---
+++ packages/common/src/utils/config.ts
@@ -0,0 +1,28 @@
+import { Platform } from 'react-native';
+interface IConfig {
+  API_URL: string;
+}
+
+const nativeConfig = Platform.select({
+  ios: () => require('react-native-config'),
+  android: () => require('react-native-config'),
+})();
+
+// get all envs from process.env
+// using ...process.env doesn't work,
+// must manually access API_URL on .env like process.env.API_URL
+const env = { ...process.env, ...nativeConfig.default };
+const envKeys = Object.keys(env)
+  .filter((item) => {
+    return item.indexOf('REACT_APP_') !== -1;
+  })
+  .reduce((acc, next) => {
+    console.log(acc, next, next.replace('REACT_APP_', ''));
+    acc[next.replace('REACT_APP_', '')] = env[next];
+    return acc;
+  }, {});
+
+const Config: Partial<IConfig> = {
+  ...envKeys,
+};
+export default Config;

6eacf57c7704a93e5c9101b6190e13b40efe7449
Chijioke Ndubisi <cjndubisi@gmail.com>
2020-05-19 21:50:21 +0100

    feat(theme): style home

--- packages/common/src/screens/Home.tsx
+++ packages/common/src/screens/Home.tsx
@@ -2,12 +2,13 @@ import React, { useContext } from 'react';
 import { View } from 'react-native';
 import { Button } from 'react-native-elements';
 import { AuthContext } from '../context/AuthContext';
+import { Container } from '../styled';
 const Home = () => {
   const { logout } = useContext(AuthContext);
   return (
-    <View>
+    <Container>
       <Button onPress={logout} title={'Logout'} />
-    </View>
+    </Container>
   );
 };
 export default Home;

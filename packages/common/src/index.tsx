9a9263fa3eb0e79818a0764ba36f5351b6d554d4
Chijioke Ndubisi <cjndubisi@gmail.com>
2020-05-21 12:04:29 +0100

    feat: navigation state restoration

--- packages/common/src/index.tsx
+++ packages/common/src/index.tsx
@@ -1,9 +1,12 @@
 import React, { useContext } from 'react';
+import { Platform, Linking } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import AllProviders from './context/AllProviders';
 import { Container } from './styled';
 import SCREENS from './screens/NavigactionConfig';
+import AsyncStorage from '@react-native-community/async-storage';
+const PERSISTENCE_KEY = 'NAVIGATION_STATE';
 
 type RootStackParamList = any;
 const App = () => {
@@ -24,11 +27,48 @@ const App = () => {
   };
 
   const Stack = createStackNavigator<RootStackParamList>();
+  const [isReady, setIsReady] = React.useState(__DEV__ ? false : true);
+  const [initialState, setInitialState] = React.useState();
 
+  React.useEffect(() => {
+    const restoreState = async () => {
+      try {
+        const initialUrl = await Linking.getInitialURL();
+
+        if (Platform.OS !== 'web' && initialUrl == null) {
+          // Only restore state if there's no deep link and we're not on web
+          const savedStateString = await AsyncStorage.getItem(PERSISTENCE_KEY);
+          const state = savedStateString
+            ? JSON.parse(savedStateString)
+            : undefined;
+
+          if (state !== undefined) {
+            setInitialState(state);
+          }
+        }
+      } finally {
+        setIsReady(true);
+      }
+    };
+
+    if (!isReady) {
+      restoreState();
+    }
+  }, [isReady]);
+
+  if (!isReady) {
+    return null;
+  }
   return (
     <Container>
       <AllProviders>
-        <NavigationContainer linking={linking}>
+        <NavigationContainer
+          linking={linking}
+          initialState={initialState}
+          onStateChange={(state) =>
+            AsyncStorage.setItem(PERSISTENCE_KEY, JSON.stringify(state))
+          }
+        >
           <Stack.Navigator>
             {Object.keys(SCREENS).map((Screen) => (
               <Stack.Screen

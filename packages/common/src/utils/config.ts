9b1e75991fdc6b8a41e7a37edf43d84271cc9c00
Chijioke Ndubisi <cjndubisi@gmail.com>
2020-05-21 15:21:25 +0100

    fix: ios cannot connect to network

--- packages/common/src/utils/config.ts
+++ packages/common/src/utils/config.ts
@@ -3,11 +3,16 @@ interface IConfig {
   API_URL: string;
 }
 import nativeConfig from './rn-confg';
+let native = nativeConfig;
+if (Platform.OS !== 'web') {
+  native = nativeConfig().default;
+}
 
 // get all envs from process.env
 // using ...process.env doesn't work,
 // must manually access API_URL on .env like process.env.API_URL
-const env = { ...process.env, ...nativeConfig };
+const env = { ...process.env, ...native };
+
 const envKeys = Object.keys(env)
   .filter((item) => {
     return item.indexOf('REACT_APP_') !== -1;
